ceb71c9b5ea6f37baf0bf1d0fb3684bba8e959fe
NiteCat <1430281848@qq.com>
2023-04-25 16:06:24 +0800

    初始化版本

---
+++ gmall-common/src/main/java/com/zr/common/GmallCommonApplication.java
@@ -0,0 +1,13 @@
+package com.zr.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallCommonApplication.class, args);
+    }
+
+}
